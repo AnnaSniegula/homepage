06028349f2f7795dacc198b6965dbc149dbf48bd
Anna Sniegula <anna.sniegula@gmail.com>
2021-05-05 20:52:45 +0200

    functions and bem added

--- js/script.js
+++ js/script.js
@@ -63,33 +63,71 @@
         console.log("Czołem przyszli programiści!")
     }
 
-
     const main__buttonRemoveHeader = document.querySelector(".main__buttonRemoveHeader");
-    const main__header = document.querySelector(".main__header");
     const main__buttonImageRemove = document.querySelector(".main__buttonImageRemove");
-    const main__photo = document.querySelector(".main__photo");
+    const main__buttonChangeColor = document.querySelector(".main__buttonChangeColor");
 
-    main__buttonImageRemove.addEventListener("click", () => {
+
+    const removeImage = () => {
+        const main__photo = document.querySelector(".main__photo");
         main__photo.classList.toggle("hidden");
-        if (main__photo.classList.contains("hidden")) {
-            main__buttonImageRemove.innerText = "Przywróć zdjęcie";
-        } else {
-            main__buttonImageRemove.innerText = "Usuń zdjęcie";
-        }
-    });
+        main__buttonImageRemove.innerText = main__photo.classList.contains("hidden") ? "Przywróć zdjęcie" :
+            "Usuń zdjęcie"
+    };
+
+
+    // main__buttonImageRemove.addEventListener("click", () => {
+    //     main__photo.classList.toggle("hidden");
+    //     main__buttonImageRemove.innerText = main__photo.classList.contains("hidden") ? "Przywróć zdjęcie" :
+    //     "Usuń zdjęcie"
+    // });
 
 
 
-    main__buttonRemoveHeader.addEventListener("click", () => {
+    // main__buttonImageRemove.addEventListener("click", () => {
+    //     main__photo.classList.toggle("hidden");
+    //     if (main__photo.classList.contains("hidden")) {
+    //         main__buttonImageRemove.innerText = "Przywróć zdjęcie";
+    //     } else {
+    //         main__buttonImageRemove.innerText = "Usuń zdjęcie";
+    //     }
+    // });
+
+
+    // main__buttonChangeColor.addEventListener("click", () => {
+    //     body.classList.toggle("body__newBackground");
+    //     main__buttonChangeColor.innerText = body.classList.contains("body__newBackground") ?
+    //         "Przywróć tło" : "Zmień kolor tła";
+    // });
+
+
+    // main__buttonRemoveHeader.addEventListener("click", () => {
+    //     main__header.classList.toggle("hidden");
+    //     main__buttonRemoveHeader.innerText = main__header.classList.contains("hidden") ?
+    //      "Przywróć nagłówek" : "Usuń nagłówek"
+
+    // });
+
+
+    const removeHeader = () => {
+        const main__header = document.querySelector(".main__header");
         main__header.classList.toggle("hidden");
-        if (main__buttonRemoveHeader.innerText === "Usuń nagłówek") {
-            main__buttonRemoveHeader.innerText = "Przywróć nagłówek";
-        }
-        else {
+        main__buttonRemoveHeader.innerText = main__header.classList.contains("hidden") ?
+            "Przywróć nagłówek" : "Usuń nagłówek"
 
-            main__buttonRemoveHeader.innerText = "Usuń nagłówek"
-        }
-    });
+    };
+
+
+    // main__buttonRemoveHeader.addEventListener("click", () => {
+    //     main__header.classList.toggle("hidden");
+    //     if (main__buttonRemoveHeader.innerText === "Usuń nagłówek") {
+    //         main__buttonRemoveHeader.innerText = "Przywróć nagłówek";
+    //     }
+    //     else {
+
+    //         main__buttonRemoveHeader.innerText = "Usuń nagłówek"
+    //     }
+    // });
 
     const toggleBackground = () => {
         const body = document.querySelector(".body");
@@ -99,9 +137,9 @@
     };
 
     const init = () => {
-        const main__buttonChangeColor = document.querySelector(".main__buttonChangeColor");
         main__buttonChangeColor.addEventListener("click", toggleBackground);
-
+        main__buttonRemoveHeader.addEventListener("click", removeHeader);
+        main__buttonImageRemove.addEventListener("click", removeImage);
     };
 
     welcome();
