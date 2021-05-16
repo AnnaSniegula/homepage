444068bbd31ca5876a649ce8dca3a19463b85b84
Anna Sniegula <anna.sniegula@gmail.com>
2021-05-16 20:48:47 +0200

    hidden name changed on main__photo--hidden

--- js/script.js
+++ js/script.js
@@ -10,15 +10,15 @@
 
     const removeImage = () => {
         const main__photo = document.querySelector(".main__photo");
-        main__photo.classList.toggle("hidden");
-        removeImageButton.innerText = main__photo.classList.contains("hidden") ? "Przywróć zdjęcie" :
+        main__photo.classList.toggle("main__photo--hidden");
+        removeImageButton.innerText = main__photo.classList.contains("main__photo--hidden") ? "Przywróć zdjęcie" :
             "Usuń zdjęcie"
     };
 
     const removeHeader = () => {
         const main__header = document.querySelector(".main__header");
-        main__header.classList.toggle("hidden");
-        removeHeaderButton.innerText = main__header.classList.contains("hidden") ?
+        main__header.classList.toggle("main__photo--hidden");
+        removeHeaderButton.innerText = main__header.classList.contains("main__photo--hidden") ?
             "Przywróć nagłówek" : "Usuń nagłówek"
 
     };
